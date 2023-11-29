1a7a976a13f90e7d9ea80ee0f876edaa8f8d9aa6
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-11-29 17:56:29 +0400

    changed origin

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ import { createNote, getNotes, getSingleNote, deleteSingleNote, updateNote} from
 import { createTask, getTasks, updateTask, deleteTask } from "./routes/tasks.js"
 
 const app = express()
-app.use(cors({origin: 'https://devweb.cc/', credentials: true})); 
+app.use(cors({
+    origin: 'https://devweb.cc',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 app.use(cookieParser()); 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
