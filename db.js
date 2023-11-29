12c8c0fe048872c8bc822eacfc0cf8d3ab43b21b
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-11-29 14:46:58 +0400

    added db keys

--- db.js
+++ db.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST,    // Replace with your MySQL server's hostname or IP address
-    user: process.env.DB_USER,     // Replace with your MySQL username
-    password: process.env.DB_PASS, // Replace with your MySQL password
-    database: process.env.DB_NAME,    // Replace with your database name
+    host: process.env.DB_HOST,    
+    user: process.env.DB_USER,     
+    password: process.env.DB_PASS, 
+    database: process.env.DB_NAME,   
   });
 
   pool.getConnection((err, connection) => {
@@ -15,6 +15,6 @@ export const pool = mysql.createPool({
       console.error('Database connection failed: ' + err.message);
     } else {
       console.log('Connected to the database');
-      connection.release(); // Release the connection back to the pool
+      connection.release(); 
     }
   });
