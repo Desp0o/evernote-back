2a71d5f8a96d8a34557aefcff8f767294090ee6a
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-11-30 15:58:47 +0400

    test fix

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/getnotes', getNotes)
 app.get('/notes/:id', getSingleNote)
 app.delete('/notes/deletenote/:id', deleteSingleNote)
 app.post('/notes/updatenote/:id', updateNote)
-app.get('/search', searchNote)
+// app.get('/search', searchNote)
 
 
 app.post('/createtask', createTask)
