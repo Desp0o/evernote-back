55dc0556a69824cd5d2e043c41b5e578a5e219ca
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-12-07 19:57:01 +0400

    access origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
     origin: ['http://localhost:1234', 'https://verdant-donut-5378cb.netlify.app', 'https://dev--verdant-donut-5378cb.netlify.app', 'https://verdant-donut-5378cb.netlify.app'],                     
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'XMLHttpRequest'],
   }));
 
 app.use(cookieParser()); 
