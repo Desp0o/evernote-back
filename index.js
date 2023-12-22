9ade9475ce946c77689f2ffece1214ac67dc529d
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-12-22 14:35:12 +0400

    changed origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { addFile, getFiles, deleteFile } from "./routes/files.js"
 
 const app = express()
 app.use(cors({
-    origin: ['http://localhost:1234', 'https://65855fb344a5bcad9cb4ebbb--iridescent-seahorse-2f6095.netlify.app'],                     
+    origin: ['http://localhost:1234', 'https://658565c94f6415b54b031680--iridescent-seahorse-2f6095.netlify.app'],                 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'XMLHttpRequest'],
